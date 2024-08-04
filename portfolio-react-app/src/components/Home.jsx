2324295ab6a0614b18032318e3e1883e7784d151
Gabriel <araujog388@gmail.com>
2024-08-04 14:11:25 -0300

    Update welcome presentation message on home page

--- portfolio-react-app/src/components/Home.jsx
+++ portfolio-react-app/src/components/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
         <p className='text-[#51c9c7]'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Gabriel</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer specializing in building exceptional experiencies. Currently, I'm focused on building responsive full-stack web applications.</p>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a student of programming and computer science, specializing in creating exceptional experiences. Currently, I'm focused on developing responsive full-stack web applications.</p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#51c9c7 hover:bg-[#51c9c7]'>
-            View Work 
+            View More
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3'/>
             </span>
