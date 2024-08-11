9d0065cfa2477c452aa6a983b26db7b36c85bc57
Gabriel <araujog388@gmail.com>
2024-08-11 16:11:30 -0300

    Added function on home page button

--- portfolio-react-app/src/components/Home.jsx
+++ portfolio-react-app/src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+import { scroller } from 'react-scroll'
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-
       {/* Container */}
       <div className='max-w-[950px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-[#51c9c7]'>Hi, my name is</p>
@@ -12,10 +12,18 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a student of programming and computer science, specializing in creating exceptional experiences. Currently, I'm focused on developing responsive full-stack web applications.</p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#51c9c7 hover:bg-[#51c9c7]'>
+          <button
+            onClick={() => scroller.scrollTo('about', {
+              duration: 1000,
+              delay: 100,
+              smooth: true,
+              itemId: 'about',
+              offset: 50,
+            })}
+            className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:border-[#51c9c7] hover:bg-[#51c9c7]'>
             View More
             <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3'/>
+              <HiArrowNarrowRight className='ml-3' />
             </span>
           </button>
         </div>
