adcb145ce8092df7adf011b798b5c2e5ef0be323
Gabriel <araujog388@gmail.com>
2024-08-11 18:13:05 -0300

    Changed mail on social icons

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#3e7569]'>
-            <a href='mailto:araujog388+portfolio@gmail.com?subject=Professional Opportunity&body=Hello, I visited your portfolio and would like to discuss an opportunity with you. Are you interested?' className='flex justify-between items-center w-full text-gray-300'>
+            <a href='mailto:gabriel.araujoprofessional+portfolio@gmail.com?subject=Professional Opportunity&body=Hello, I visited your portfolio and would like to discuss an opportunity with you. Are you interested?' className='flex justify-between items-center w-full text-gray-300'>
             Email <HiOutlineMail size={30} />
             </a>
           </li>
