46d090f0f36b74a1860348a78bbcca31f1714574
Gabriel <araujog388@gmail.com>
2024-08-20 19:17:39 -0300

    Update text in Home.jsx

--- portfolio-react-app/src/components/Home.jsx
+++ portfolio-react-app/src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
       <div className='max-w-[950px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-[#51c9c7]'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Gabriel</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a student of programming and computer science, specializing in creating exceptional experiences. Currently, I'm focused on developing responsive full-stack web applications.</p>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a student of computer science and programming, currently focused on developing responsive web applications.</p>
         <div>
           <button
             onClick={() => scroller.scrollTo('about', {
